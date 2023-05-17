ec24b8f6ebbf0fb22f36b13b145ef43dbeeb558e
aayushchauhan10 <ai.aayush10@gmail.com>
2023-05-17 17:36:32 +0530

    Updated Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -16,7 +16,7 @@ const Link = ({ icon, description }) => (
 
 const Header = () => {
   return (
-    <div className="myHead">
+    <div id="myHead">
       <img src={bannerImage} className="-z-50" />
       <div className="absolute top-0 left-0">
         <img src={headerLogo} className="p-[60px] w-[320px] h-48" />
